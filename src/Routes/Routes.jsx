95f4e080b7d1f9d6a52932164c890f3ce97a8d41
sajib-rana <sajibrana444@gmail.com>
2023-08-20 00:17:38 +0600

    navbar

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,6 +6,7 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 
 
+
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -15,6 +16,7 @@ export const router = createBrowserRouter([
             path: '/',
             element: <Home></Home>
         }
+        
     ]
   },
 ]);
