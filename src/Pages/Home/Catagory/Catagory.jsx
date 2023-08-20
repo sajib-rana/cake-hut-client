7998e6f3a219b6bdbe94eb87ada5523a1ea20bdd
sajib-rana <sajibrana444@gmail.com>
2023-08-20 12:45:45 +0600

    slide in home page

---
+++ src/Pages/Home/Catagory/Catagory.jsx
@@ -0,0 +1,34 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import slide1 from '../../../assets/assets/home/slide1.jpg'
+import slide2 from '../../../assets/assets/home/slide2.jpg'
+import slide3 from '../../../assets/assets/home/slide3.jpg'
+import slide4 from '../../../assets/assets/home/slide4.jpg'
+import slide5 from '../../../assets/assets/home/slide5.jpg'
+
+const Catagory = () => {
+    return (
+        <>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src={slide1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide2} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide3} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide4} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide5} alt="" /></SwiperSlide>
+      </Swiper>
+    </>
+    );
+};
+
+export default Catagory;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Banner from "../Banner/Banner";
+import Catagory from "../Catagory/Catagory";
+
+
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Catagory></Catagory>
         </div>
     );
 };
