5ca56e265b9e8f424e14976eaf836cc2fb0e5321
sajib-rana <sajibrana444@gmail.com>
2023-08-20 16:02:28 +0600

    section tittle add

--- src/Pages/Home/Catagory/Catagory.jsx
+++ src/Pages/Home/Catagory/Catagory.jsx
@@ -7,11 +7,18 @@ import slide2 from '../../../assets/assets/home/slide2.jpg'
 import slide3 from '../../../assets/assets/home/slide3.jpg'
 import slide4 from '../../../assets/assets/home/slide4.jpg'
 import slide5 from '../../../assets/assets/home/slide5.jpg'
+import slide6 from '../../../assets/assets/home/slide6.jpg'
+import SectionTitle from '../../../Components/SectionTittle/SectionTittle';
 
 const Catagory = () => {
     return (
         <>
-      <Swiper
+      <section>
+        <SectionTitle 
+            heading={"Order Online"}
+            subHeading={"From 11.00am to 10.00pm"}
+            ></SectionTitle>
+        <Swiper
         slidesPerView={4}
         spaceBetween={30}
         centeredSlides={true}
@@ -19,14 +26,16 @@ const Catagory = () => {
           clickable: true,
         }}
         modules={[Pagination]}
-        className="mySwiper"
+        className="mySwiper mb-24"
       >
         <SwiperSlide><img src={slide1} alt="" /></SwiperSlide>
         <SwiperSlide><img src={slide2} alt="" /></SwiperSlide>
         <SwiperSlide><img src={slide3} alt="" /></SwiperSlide>
         <SwiperSlide><img src={slide4} alt="" /></SwiperSlide>
         <SwiperSlide><img src={slide5} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide6} alt="" /></SwiperSlide>
       </Swiper>
+      </section>
     </>
     );
 };
